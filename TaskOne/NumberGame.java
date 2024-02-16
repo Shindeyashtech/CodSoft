4adad7b10609e953b0782e826f4ca658686079e8
Shindeyashtech <yashshinde.tech@gmail.com>
2024-02-16 23:16:27 +0530

    StageOne Done

--- TaskOne/NumberGame.java
+++ TaskOne/NumberGame.java
@@ -1,9 +1,10 @@
-/**
- * NumberGame
- */
+ import java.util.*;
 public class NumberGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome To NUMic ");
+        Random random = new Random();
+        int randomNum = random.nextInt(101);
+        System.out.println(randomNum);
     }
 }
