5500aefc5b6514e34c8931ca596bb57eb640e8f2
Shindeyashtech <yashshinde.tech@gmail.com>
2024-02-22 00:01:25 +0530

    Display Message

--- TaskSecond/GradeCal.java
+++ TaskSecond/GradeCal.java
@@ -1,10 +1,14 @@
-/**
- * GradeCal
- */
+ import java.util.*;
 public class GradeCal {
 
     public static void main(String[] args) {
-        System.out.println("WWelcome to student credit calculator");
+        System.out.println("WWelcome to student Grade calculator");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter you Name");
+        String name = sc.next();
+        System.out.println("Hey!!  "+name + " Welcome to Grade Calculator" );
+
+
         
     }
 }
