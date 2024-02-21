197f7204cae070e1d450535d16de7c9a33a80aac
Shindeyashtech <yashshinde.tech@gmail.com>
2024-02-21 23:38:18 +0530

    TaskTwo

---
+++ TaskSecond/GradeCal.java
@@ -0,0 +1,10 @@
+/**
+ * GradeCal
+ */
+public class GradeCal {
+
+    public static void main(String[] args) {
+        System.out.println("WWelcome to student credit calculator");
+        
+    }
+}
