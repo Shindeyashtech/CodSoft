d5ad0c40274463a41c4602e6760901b5bcd9e700
Shindeyashtech <yashshinde.tech@gmail.com>
2024-02-23 10:09:44 +0530

    userinput for marks

--- TaskSecond/GradeCal.java
+++ TaskSecond/GradeCal.java
@@ -2,13 +2,91 @@
 public class GradeCal {
 
     public static void main(String[] args) {
-        System.out.println("WWelcome to student Grade calculator");
         Scanner sc = new Scanner(System.in);
+        System.out.println("WWelcome to student Grade calculator");
         System.out.println("Enter you Name");
         String name = sc.next();
         System.out.println("Hey!!  "+name + " Welcome to Grade Calculator" );
-
-
+        System.out.println("Enter your English Marks" );
+        double englishGrade= sc.nextDouble();
+        System.out.println("Enter your  Marathi Marks" );
+        double marathiGrade= sc.nextDouble();
+        System.out.println("Enter your  Hindi Marks" );
+        double hindiGrade= sc.nextDouble();
+        System.out.println("Enter your  Math Marks" );
+        double mathGrade= sc.nextDouble();
+        System.out.println("Enter your  Science Marks" );
+        double scienceGrade= sc.nextDouble();
         
+        Grade  =new Grade();
+
+
     }
+}
+/**
+* Grade
+ */
+class Grade {
+ 
+    private double math,science,hindi,english,marathi,evs;
+
+    Grade(){
+        this.math = math;
+        this.science = science;
+        this.hindi = hindi;
+        this.marathi = marathi;
+        this.english = english;
+        this.evs = evs;
+    }
+
+    public double getMath() {
+        return math;
+    }
+
+    public void setMath(double math) {
+        this.math = math;
+    }
+
+    public double getScience() {
+        return science;
+    }
+
+    public void setScience(double science) {
+        this.science = science;
+    }
+
+    public double getHindi() {
+        return hindi;
+    }
+
+    public void setHindi(double hindi) {
+        this.hindi = hindi;
+    }
+
+    public double getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(double english) {
+        this.english = english;
+    }
+
+    public double getMarathi() {
+        return marathi;
+    }
+
+    public void setMarathi(double marathi) {
+        this.marathi = marathi;
+    }
+
+    public double getEvs() {
+        return evs;
+    }
+
+    public void setEvs(double evs) {
+        this.evs = evs;
+    }
+    
+    
+    
 }
