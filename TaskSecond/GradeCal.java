ab70860873213197adc2a1bcfb81f3bbb95bb89b
Shindeyashtech <yashshinde.tech@gmail.com>
2024-02-23 10:55:12 +0530

    input storing in variables and debugging

--- TaskSecond/GradeCal.java
+++ TaskSecond/GradeCal.java
@@ -1,42 +1,18 @@
- import java.util.*;
-public class GradeCal {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("WWelcome to student Grade calculator");
-        System.out.println("Enter you Name");
-        String name = sc.next();
-        System.out.println("Hey!!  "+name + " Welcome to Grade Calculator" );
-        System.out.println("Enter your English Marks" );
-        double englishGrade= sc.nextDouble();
-        System.out.println("Enter your  Marathi Marks" );
-        double marathiGrade= sc.nextDouble();
-        System.out.println("Enter your  Hindi Marks" );
-        double hindiGrade= sc.nextDouble();
-        System.out.println("Enter your  Math Marks" );
-        double mathGrade= sc.nextDouble();
-        System.out.println("Enter your  Science Marks" );
-        double scienceGrade= sc.nextDouble();
-        
-        Grade  =new Grade();
-
-
-    }
-}
-/**
-* Grade
- */
+import java.util.*;
 class Grade {
- 
-    private double math,science,hindi,english,marathi,evs;
 
-    Grade(){
-        this.math = math;
-        this.science = science;
-        this.hindi = hindi;
-        this.marathi = marathi;
-        this.english = english;
-        this.evs = evs;
+    private double math, science, hindi, english, marathi, evs;
+
+    public Grade() {
+    }
+
+    void printMarks() {
+        System.out.println(" English" + english);
+        System.out.println(" Marathi" + marathi);
+        System.out.println(" hindi" + hindi);
+        System.out.println(" Math" + math);
+        System.out.println(" Science" + science);
+        System.out.println(" EVS" + evs);
     }
 
     public double getMath() {
@@ -86,7 +62,48 @@ class Grade {
     public void setEvs(double evs) {
         this.evs = evs;
     }
-    
-    
-    
+
 }
+
+
+
+public class GradeCal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to student Grade calculator");
+        System.out.println("Enter you Name");
+        String name = sc.next();
+        System.out.println("Hey!!  " + name + " Welcome to Grade Calculator");
+
+        System.out.println("Enter your English Marks");
+        double englishGrade = sc.nextDouble();
+
+        System.out.println("Enter your  Marathi Marks");
+        double marathiGrade = sc.nextDouble();
+
+        System.out.println("Enter your  Hindi Marks");
+        double hindiGrade = sc.nextDouble();
+
+        System.out.println("Enter your  Math Marks");
+        double mathGrade = sc.nextDouble();
+
+        System.out.println("Enter your  Science Marks");
+        double scienceGrade = sc.nextDouble();
+
+        System.out.println("Enter your  EVS Marks");
+        double evsGrade = sc.nextDouble();
+
+        sc.close();
+        Grade marksObtain = new Grade();
+        marksObtain.setEnglish(englishGrade);
+        marksObtain.setMarathi(marathiGrade);
+        marksObtain.setHindi(hindiGrade);
+        marksObtain.setMath(mathGrade);
+        marksObtain.setScience(scienceGrade);
+        marksObtain.setEvs(evsGrade);
+
+        marksObtain.printMarks();
+
+    }}
+
